dab0856e5e1454fce3dcc1d851675d616a0f5a57
vac-end <sc220404@gmail.com>
2025-10-15 22:54:00 -0500

    feat: Update routes to include lesson, course content, enrollment, schedule, and quiz routes

--- src/shared/interfaces/routes.ts
+++ src/shared/interfaces/routes.ts
@@ -5,6 +5,11 @@ import attendanceRoutes from '@features/Attendance/attendance.routes'
 import academicLevelRoutes from '@features/AcademicLevel/academicLevel.routes'
 import courseRoutes from '@features/course/course.routes'
 import moduleRoutes from '@features/Module/module.routes'
+import lessonRoutes from '@features/Lesson/lesson.routes'
+import courseContentRoutes from '@features/courseContent/courseContent.routes'
+import enrollmentRoutes from '@features/Enrollment/enrollment.routes'
+import scheduleRoutes from '@features/Schedule/schedule.routes'
+import quizRoutes from '@features/Quiz/quiz.routes'
 
 const router = Router();
 
@@ -13,15 +18,11 @@ router.use('/academicLevel', academicLevelRoutes);
 router.use('/users', userRoutes);
 router.use('/courses', courseRoutes);
 router.use('/modules', moduleRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
-router.use('/users', userRoutes);
+router.use('/lessons', lessonRoutes);
+router.use('/courseContents', courseContentRoutes);
+router.use('/enrollment', enrollmentRoutes);
 router.use('/attendances', attendanceRoutes)
+router.use('/schedules', scheduleRoutes);
+router.use('/quizs', quizRoutes);
 
 export default router;
